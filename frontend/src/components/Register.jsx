9a0ab2c67e1254b92fbae99b8b760ee0122fbb73
Raymond Oluoch <rasholuoch@gmail.com>
2023-05-17 18:13:34 +0300

    created state and register page

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from "react-redux"
+import { userRegister } from '../store/actions/authAction';
 
 const Register = () => {
+  const dispatch = useDispatch();
+
+  const [state, setstate] = useState({
+    userName : '',
+    email:'',
+    password:'',
+    confirmPassword : '',
+    image : ''
+  })
+
+  const [loadImage, setLoadImage] = useState('');
+
+  const inputHendle = e => {
+    setstate({
+      ...state,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const fileHendle = e =>{
+    if(e.target.files.length !==0){
+      setstate({
+        ...state,
+        [e.target.name] : e.target.files[0]
+      })
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setLoadImage(reader.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
+
+  const register = e =>{
+    e.preventDefault();
+
+    const { userName, email, password, confirmPassword, image } = state;
+
+    const formData = new FormData();
+    formData.append('userName', userName);
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('confirmPassword', confirmPassword);
+    formData.append('image', image);
+
+    dispatch(userRegister(formData));
+  }
+
   return (
     <div className='register'>
       <div className='card'>
@@ -10,34 +61,34 @@ const Register = () => {
         </div>
 
         <div className='card-body'>
-          <form>
+          <form onSubmit={register}>
             <div className='form-group'>
               <label htmlFor='username'>User Name</label>
-              <input type="text" name="userName" className='form-control' placeholder='User Name' id='username' />
+              <input type="text" name="userName" onChange={inputHendle} value={state.userName} className='form-control' placeholder='User Name' id='username' />
             </div>
 
             <div className='form-group'>
               <label htmlFor='email'>Email</label>
-              <input type="email" name="email" className='form-control' placeholder='Email' id='email' />
+              <input type="email" name="email" onChange={inputHendle} value={state.email} className='form-control' placeholder='Email' id='email' />
             </div>
 
             <div className='form-group'>
               <label htmlFor='password'>Password</label>
-              <input type="password" name="password" className='form-control' placeholder='Password' id='password' />
+              <input type="password" name="password" onChange={inputHendle} value={state.password} className='form-control' placeholder='Password' id='password' />
             </div>
 
             <div className='form-group'>
               <label htmlFor='confirmPassword'>Confirm Password</label>
-              <input type="password" name="confirmPassword" className='form-control' placeholder='Confirm Password' id='confirmPassword' />
+              <input type="password" name="confirmPassword" onChange={inputHendle} value={state.confirmPassword} className='form-control' placeholder='Confirm Password' id='confirmPassword' />
             </div>
 
             <div className='form-group'>
               <div className='file-image'>
-                <div className='image'></div>
+                <div className='image'>{loadImage ? <img src={loadImage} alt='profile-avatar' /> : ''  } </div>
 
                 <div className='file'>
                   <label htmlFor='image'>Select Image</label>
-                  <input type="file" name="image" className='form-control' id='image' />
+                  <input type="file" name="image" onChange={fileHendle} className='form-control' id='image' />
                 </div>
               </div>
             </div>
