400354a80f0429db88d85543252e6ec163bba27b
Raymond Oluoch <rasholuoch@gmail.com>
2023-05-18 12:54:34 +0300

    user backend authentication

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
-const databaseConnect = require('./config/database')
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const databaseConnect = require("./config/database");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const authRouter = require('./routes/authRoute')
 
 dotenv.config({
-  path: 'backend/config/config.env'
-})
+  path: "backend/config/config.env",
+});
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 5000
-app.get('/', (req, res)=>{
-  res.send('This is a Real Time Chat App API server')
-})
+app.use('/api/chat',authRouter);
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("This is a Real Time Chat App API server");
+});
 
 databaseConnect();
 
-app.listen(PORT, ()=>{
-  console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
