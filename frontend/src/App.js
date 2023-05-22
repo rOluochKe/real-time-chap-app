b747bc3c15bfb95fa42fa486e12f8aff06350969
Raymond Oluoch <rasholuoch@gmail.com>
2023-05-22 13:04:58 +0300

    socket and messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/ProtectRoute";
 
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Messenger from "./components/Messenger";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/chat/login" element={<Login />} />
         <Route path="/chat/register" element={<Register />} />
+        <Route path="/" element={ <ProtectRoute> <Messenger /> </ProtectRoute> } />
       </Routes>
     </BrowserRouter>
   );
